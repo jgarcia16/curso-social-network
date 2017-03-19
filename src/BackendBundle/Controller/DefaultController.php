f1e8c6f566dc5ff6ef77b69b3cc521b2feb5dca4
KillerBeeKB <quini.emancipia@gmail.com>
2017-03-19 12:03:46 +0100

    funcionando y consulta de usuario

--- src/BackendBundle/Controller/DefaultController.php
+++ src/BackendBundle/Controller/DefaultController.php
@@ -8,6 +8,14 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em=$this->getDoctrine()->getManager();
+        $user_repo=$em->getRepository("BackendBundle:User");
+        $user=$user_repo->find(1);
+        
+       echo $user->getName()." ".$user->getEmail();
+        //var_dump($user);
+        die();
         return $this->render('BackendBundle:Default:index.html.twig');
+        
     }
 }
