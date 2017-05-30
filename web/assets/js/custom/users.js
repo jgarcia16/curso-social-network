77506e7297e0bcd4df733799d119e08aeb96830f
KillerBeeKB <quini.emancipia@gmail.com>
2017-05-30 23:23:08 +0200

    Clase 042

--- web/assets/js/custom/users.js
+++ web/assets/js/custom/users.js
@@ -24,7 +24,9 @@ $(document).ready(function(){
     
     function followButtons(){
        $(".btn-follow").unbind("click").click(function(){
-                   $.ajax({
+                $(this).addClass("hidden");
+                $(this).parent().find(".btn-unfollow").removeClass("hidden");
+                $.ajax({
                        url: URL+'/follow',
                        type: 'POST',
                        data: {followed: $(this).attr("data-followed")},
@@ -36,7 +38,10 @@ $(document).ready(function(){
        });
        
             $(".btn-unfollow").unbind("click").click(function(){
-                   $.ajax({
+                $(this).addClass("hidden");
+                $(this).parent().find(".btn-follow").removeClass("hidden");
+                
+                    $.ajax({
                        url: URL+'/unfollow',
                        type: 'POST',
                        data: {followed: $(this).attr("data-followed")},
