e15713dad25927dbf9f681c89d34584005724112
KillerBeeKB <quini.emancipia@gmail.com>
2017-06-16 17:37:55 +0200

    Curso 045

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -6,10 +6,19 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use BackendBundle\Entity\Publication;
+use AppBundle\Form\PublicationType;
+
 class PublicationController extends Controller
 {
       public function indexAction(Request $request){
-           return $this->render('AppBundle:Publication:home.html.twig');
+           $em=$this->getDoctrine()->getManager();
+		   $publication=new Publication();
+		   $form= $this->createForm(PublicationType::class, $publication);
+
+		   return $this->render("AppBundle:Publication:home.html.twig",array(
+				'form' => $form->createView()
+		   ));
     }
 }
 
